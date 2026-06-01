fbad9a49b2d4d721274c1ea0097d1dbe24737413
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-06-01 17:30:51 +0200

    user.js uppdaterad med kod från labb 4

--- models/user.js
+++ models/user.js
@@ -1,8 +1,76 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+//Bestämmer hur en användare ska se ut i databasen
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true, //Kan bara finnas en användare med det namnet
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model("User", UserSchema);
+//En pre-save funktion som körs innan det sparas i databasen
+userSchema.pre("save", async function () {
+  try {
+    //Om lösenordet är nytt eller ändrats så ska det krypteras
+    if (this.isNew || this.isModified("password")) {
+      const hashedPassword = await bcrypt.hash(this.password, 10); // Hashar lösenordet så  det ej sparas i klartext
+      this.password = hashedPassword;
+    }
+  } catch (error) {
+    throw error;
+  }
+});
+
+//Funktion för att skapa en ny användare
+userSchema.statics.register = async function (username, password) {
+  try {
+    const user = new this({ username, password });
+    await user.save();
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+//Jämför det inskrivna lösenrodet med det krypterade i databasen
+userSchema.methods.comparePassword = async function (password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    throw error;
+  }
+};
+
+//Logik för själva inloggningen
+userSchema.statics.login = async function (username, password) {
+  try {
+    //Kolla om användaren finns i databasen
+    const user = await this.findOne({ username });
+    if (!user) {
+      throw new Error("Incorrect username/password");
+    }
+    //Om användare finns kolla om lösenordet matchar
+    const isPasswordMatch = await user.comparePassword(password);
+    if (!isPasswordMatch) {
+      throw new Error("Incorrect username/password");
+    }
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
